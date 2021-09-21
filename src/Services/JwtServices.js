4a1f3eca774c8a73afcb6ba56fc51b9ffc4dae71
João Dias <joao.dias@somagrupo.com.br>
2021-09-20 21:24:53 -0300

    Começa lógica do jwt

--- src/Services/JwtServices.js
+++ src/Services/JwtServices.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
+require('dotenv/config');
 
-const generateToken = async (email, name) => await jwt.sign({ email, name }, {expiresIn: 60 * 60 * 0.5})
+const generateToken = async (email, name) => await jwt.sign({ email, name },process.env.JW__SECRET ,  {expiresIn: 60 * 60 * 0.5})
 
 
 module.exports = {
